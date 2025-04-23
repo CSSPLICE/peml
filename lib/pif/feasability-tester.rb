ce823eb258df576697b0cad38fd2c96954341c93
Min2505 <mintran7077@gmail.com>
2025-04-23 09:05:30 -0400

    Add PIF hash examples.

--- lib/pif/feasability-tester.rb
+++ lib/pif/feasability-tester.rb
@@ -5,6 +5,7 @@ require_relative "parsons"
 
 examples_path = "#{File.dirname(File.expand_path(__FILE__))}/feasability-examples"
 pif_to_parsons_path = "#{File.dirname(File.expand_path(__FILE__))}/pif-to-parsons"
+pif_hashes_path = "#{File.dirname(File.expand_path(__FILE__))}/pif-hashes"
 
 Dir.glob('*', base: examples_path).each do |filename|
   content = File.read("#{examples_path}/#{filename}")
@@ -15,5 +16,9 @@ Dir.glob('*', base: examples_path).each do |filename|
     File.open("#{pif_to_parsons_path}/#{filename}.txt", 'w') do |file|
       PP.pp(Parsons.convert_PIF(parsed[:value]), file)
     end
+  end
+
+  File.open("#{pif_hashes_path}/#{filename}.txt", 'w') do |file|
+    PP.pp(parsed, file)
   end
 end
--- lib/pif/parser.rb
+++ lib/pif/parser.rb
@@ -316,4 +316,5 @@ module Parser
   
     return norms, blocklists, distractors
   end
+
 end
