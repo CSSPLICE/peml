218c55fefaa16e8b20c73956c7b69fd58389721a
Kwasi Biritwum-Nyarko <kwasimbnyarko@gmail.com>
2025-05-31 18:19:22 -0400

    ruby 2 ~> 3 argument test

--- lib/peml.rb
+++ lib/peml.rb
@@ -11,7 +11,7 @@ module Peml
 
   #~ Class methods ...........................................................
   # -------------------------------------------------------------
-  def self.parse(params = {}, language: nil)
+  def self.parse(language: nil, **params)
     if params[:filename]
       file = File.open(params[:filename])
       begin
@@ -23,7 +23,7 @@ module Peml
       peml = params[:peml]
     end
     value = Peml::Loader.new.load(peml)
-    #Should we provide a param to render test cases?
+    # Should we provide a param to render test cases?
     value = Peml::DatadrivenTestRenderer.new.generate_tests(value)
     if !params[:result_only]
       diags = validate(value)
@@ -44,13 +44,11 @@ module Peml
     end
   end
 
-
   # -------------------------------------------------------------
   # Validate a PEML data structure (parsed PEML structured as a nested hash)
   def self.validate(peml)
     Utils.unpack_schema_diagnostics(Utils.schema.validate(peml))
   end
-
 
   # -------------------------------------------------------------
   # inline external file contents in fields inside
@@ -59,7 +57,6 @@ module Peml
   def self.inline(peml)
     peml
   end
-
 
   # -------------------------------------------------------------
   # handle mustache variable interpolation in fields inside
@@ -70,7 +67,6 @@ module Peml
     Utils.handle_exclusion(default_peml, Utils.recurse_hash(peml, :interpolate_helper, default_peml).dottie!)
   end
 
-
   # -------------------------------------------------------------
   # convert markdown or other markup formats to html in fields inside
   # a PEML data structure (parsed PEML structured as a nested hash)
@@ -78,7 +74,6 @@ module Peml
   def self.render_to_html(peml)
     Utils.recurse_hash(peml, :render_helper, {})
   end
-
 
   # -------------------------------------------------------------
   # parse PEMLtest text input into a data structure
@@ -94,7 +89,6 @@ module Peml
     Peml::PemlTestParser.new.parse(pemltest)
   end
 
-
   # -------------------------------------------------------------
   # render (unparse) a PEML data structure (parsed PEML structured as a
   # nested hash) into plain-text PEML notation
@@ -108,7 +102,6 @@ module Peml
     # or filename as {filename:"./file.peml"}
     PifParser.parse(pif)
   end
-
 
   # parsed_pif should be a product of pif.parse
   # format options are 'json' and 'yaml'.
